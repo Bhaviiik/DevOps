3f3450d144f3b66c254a6d1e7af7dfe2e1c2672b
Bhaviiik <103363838+Bhaviiik@users.noreply.github.com>
2023-01-18 17:40:43 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+      System.out.println("Patel Bhavik");
+      System.out.println("202051134");
+    }
+}
